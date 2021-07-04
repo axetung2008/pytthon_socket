fd8aa58ea0980951d436989ab62805e935932824
Gnut <axetung2008@gmail.com>
2021-07-04 17:19:59 +0700

    config bot

--- bot/mybot.py
+++ bot/mybot.py
@@ -1,10 +1,44 @@
 import telebot
 import os
+import subprocess
 
 bot = telebot.TeleBot("1880360296:AAFzuqt9LHjps6czoU0-WA4i2VNarR7cjkk", parse_mode=None)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "/ram ==== display the total amount of memory"
+					+"\n"+"/os  ==== display version OS"
+					+"\n"+"/hw  ==== displays the amount of disk space")
+
+@bot.message_handler(commands=['os'])
+def display_version_os(message):
+	commands = ['cat','/etc/os-release'] 
+	result = subprocess.check_output(commands,stderr=subprocess.STDOUT)
+	bot.reply_to(message, result.decode())
+
+
+#kiem tra RAM
+@bot.message_handler(commands=['ram'])
+def display_total_ram(message):
+	commands = ['free','-h']
+	result = subprocess.check_output(commands,stderr=subprocess.STDOUT)
+	bot.reply_to(message, result.decode())
+
+@bot.message_handler(commands=['hw'])
+def display_disk_space(message):
+	commands = ['df','-h']
+	result = subprocess.check_output(commands,stderr=subprocess.STDOUT)
+	bot.reply_to(message, result.decode())
+
+# def stock_request(message):
+# 	request = message.text.split()
+# 	if len(request) < 2 or request[0].lower() not in "test":
+# 		return False
+# 	else:
+# 		return True
+# @bot.message_handler(func=stock_request)
+# def send_price(message):
+# 	request = message.text.split()
+# 	print(request)
 
 bot.polling()
