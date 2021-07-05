c92447cf1670395634ab6b6d067652f46f8e1d25
gnut <axetung2008@gmail.com>
2021-07-05 15:19:01 +0700

    config bot

--- bot/mybot.py
+++ bot/mybot.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 
 bot = telebot.TeleBot("1880360296:AAFzuqt9LHjps6czoU0-WA4i2VNarR7cjkk", parse_mode=None)
-
+chat_id = 1537849994
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "/ram ==== display the total amount of memory"
@@ -30,6 +30,19 @@ def display_disk_space(message):
 	result = subprocess.check_output(commands,stderr=subprocess.STDOUT)
 	bot.reply_to(message, result.decode())
 
+def tree_dir(message):
+	request = message.text.split()
+	if len(request) < 2 or request[0].lower() not in "tree":
+		return False
+	else:
+		return True
 
+@bot.message_handler(func=tree_dir)
+def res_tree(message):
+	directory = message.text.split()[1]
+	request = ['tree',directory,'-o','direc_tree.txt']
+	result = subprocess.check_output(request,stderr=subprocess.STDOUT)
+	doc = open('direc_tree.txt','rb')
+	bot.send_document(chat_id, doc)
 
 bot.polling()
