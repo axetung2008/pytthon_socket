205b8cbb7db106335872b0fa0c5bffa0febe5554
Gnut <axetung2008@gmail.com>
2021-07-10 21:38:31 +0700

    read file

--- readfile/openfile.py
+++ readfile/openfile.py
@@ -3,10 +3,25 @@ f = open("direc_tree.txt", "r")
 
 for x in f:
 
-	print(x)
+	# print(x)
 	# a = re.findall("\Aâ”œâ”€â”€",x)
 	# if a:
 	# 	print(x)
+	try:
+
+		a = x.split("]  /")
+		print(a[1])
+	
+	except IndexError as e:
+		pass
+
+	# a = re.search(r"/etc",x)
+	# print(a.string)
+
+	# a = re.findall("^].*\n$")
+	# if a:
+	# 	print
+
 
 # con trực tiếp â””â”€â”€
 # con của con â”‚Â Â  â”œâ”€â”€
