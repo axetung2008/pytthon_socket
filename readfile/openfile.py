e008252609b9d5d9e282df6a99205e51bd024082
gnut <axetung2008@gmail.com>
2021-07-10 12:22:58 +0700

    text

--- readfile/openfile.py
+++ readfile/openfile.py
@@ -1,3 +1,12 @@
+import re
 f = open("direc_tree.txt", "r")
+root = ""
 for x in f:
- 	print(x)
+	# a = re.findall("\A/",x)
+	# if a:
+	# 	root = x	
+	print(x)
+	# a = re.findall("\Aâ”œâ”€â”€",x)
+	# if a:
+	# 	print(x)
+
