94d099a029151069439ac34fc804738159e62b7c
Gnut <axetung2008@gmail.com>
2021-07-21 21:55:56 +0700

    timer

--- timer/time.py
+++ timer/time.py
@@ -2,13 +2,13 @@ import time as t
 import socket
 ##this will enable to utilize specified functions within time library such as sleep()
 #time countdown 
-seconds = 30
+seconds = 5
 
 def port():
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	address = '168.138.53.103'
 
-	port = [80,43,2222]
+	port = [25,80,43,2222,465]
 	for p in port:
 		result = sock.connect_ex((address,p))
 		if result == 0:
@@ -19,10 +19,11 @@ def port():
 		   print("{} is not open".format(p))
 		sock.close()
 
-while True:
-	for i in range(seconds):
-		print(str(seconds-i) + " seconds remaining \n")
-		t.sleep(1)
-	port()
-	print("Time is up")
+# while True:
+# 	for i in range(seconds):
+# 		print(str(seconds-i) + " seconds remaining \n")
+# 		t.sleep(1)
+# 	port()
+# 	print("Time is up")
 ##we also need the loop to wait for 1 second between each iteration
+port()
